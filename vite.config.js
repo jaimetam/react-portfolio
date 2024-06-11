53c515067e048eb16cab994793c1589b75e3222a
jaimetam <tamesjaime96@gmail.com>
2024-06-10 21:54:40 -0600

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,4 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-  build: {
-    outDir: "dist",
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"), // Set up alias for cleaner imports
-    },
-  },
-  // Optional: Configure public base path if deploying to a subdirectory
-  base: process.env.NODE_ENV === "production" ? "/React-portafolio/" : "/", 
 });
